596cc676b80bd8a197676eb3237168d8bee48760
Trym Todalshaug <trym@digitools.no>
2018-07-25 12:05:27 +0200

    updated command

--- pumpit.js
+++ pumpit.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js');
+const Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 const DBL = require('dblapi.js');
 
-const client = new Discord.Client();
-const dbl = new DBL(auth.token, client);
+//const client = new Discord.Client();
+//const dbl = new DBL(auth.token, client);
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -25,16 +25,22 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-
-bot.on('message', message => {
-    
-    logger.info("message", message)
-
-    if (message === '!pumpit') {
-        message.channel.send("Yo", {
-            files: [
-                "https://cdn.discordapp.com/attachments/145942475805032449/471311185782898698/pumpItInTheClub.gif"
-            ]
-        });
-    }
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'pumpit':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "https://cdn.discordapp.com/attachments/145942475805032449/471311185782898698/pumpItInTheClub.gif"
+                });
+            break;
+         }
+     }
 });
